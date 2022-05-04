88cddf9d79411503e42cfea7cb946d59252d7657
Oleg Zavrazhin <olegzavrazhin@yahoo.com>
2022-05-04 23:56:32 +0300

    openApi generators and main structure to generate API models

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,24 @@
 rootProject.name = "easystory-202202"
-include("m1l1")
-include("m1l2")
-include("m1l4-dsl")
-include("main-project")
-include("main-project-transport-main-openapi")
+
+pluginManagement {
+    plugins {
+//        settings are being set in gradle.properties
+        val kotlinVersion: String by settings
+        val kotestVersion: String by settings
+        val openapiVersion: String by settings
+//        val serializationVersion: String by settings
+
+//        apply false means not to apply to current project and use the plugins in subprojects without version
+        kotlin("jvm") version kotlinVersion apply false
+        kotlin("multiplatfrom") version kotlinVersion apply false
+        id("io.kotest.multiplatform") version kotestVersion apply false
+        kotlin("plugin.serialization") version kotlinVersion apply false
+
+        id("org.openapi.generator") version openapiVersion apply false
+    }
+}
+
+//include("m1l1")
+//include("m1l2")
+//include("m1l4-dsl")
+include("easystory-api-kmp")
