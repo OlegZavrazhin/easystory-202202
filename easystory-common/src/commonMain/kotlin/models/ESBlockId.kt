277bbf60278ff77154f15bafaf2708477db5bfad
Oleg Zavrazhin <olegzavrazhin@yahoo.com>
2022-05-30 23:56:47 +0300

    fixed after review

--- easystory-common/src/commonMain/kotlin/models/ESBlockId.kt
+++ easystory-common/src/commonMain/kotlin/models/ESBlockId.kt
@@ -1,8 +1,8 @@
 package ru.otus.otuskotlin.easystory.common.models
 
 @JvmInline
-value class ESBlockId(private val id: String) {
-    fun asInt() = id.toInt()
+value class ESBlockId(private val id: String?) {
+    fun asIntOrNull() = id?.toInt()
 
     companion object {
         val NONE = ESBlockId("")
