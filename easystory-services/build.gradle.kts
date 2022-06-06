2d0be8ebefff08cc0f70fb3012b9867c0b6e88aa
Oleg Zavrazhin <olegzavrazhin@yahoo.com>
2022-06-07 01:05:35 +0300

    gradles

---
+++ easystory-services/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    kotlin("multiplatform")
+}
+
+kotlin {
+    jvm {}
+//    macosX64 {}
+//    linuxX64 {}
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation(kotlin("stdlib-common"))
+
+                implementation(project(":easystory-common"))
+                implementation(project(":easystory-stubs"))
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test-common"))
+                implementation(kotlin("test-annotations-common"))
+            }
+        }
+    }
+}
